f2e3d2c1d567458e706a4ae99ee9973397b986a8
Robert Donaway <rldonaway@gmail.com>
2019-06-23 18:51:40 -0700

    stopped vehicles are now showing in the table

--- web/sfmta/location.py
+++ web/sfmta/location.py
@@ -1,11 +1,15 @@
+import re
+import sys
 from flask import Blueprint
 from flask import render_template
-from sfmta.models import Vehicle
 from flask import flash
 from flask import redirect
 from flask import request
 from flask import url_for
-import re
+
+from sfmta.db import db_session
+
+from sfmta.models import Vehicle
 
 bp = Blueprint("location", __name__)
 dt_regexp = "^201\d{1}-[01]\d-[012]\d [012]\d:[0-5]\d:[0-5]\d$"
@@ -36,11 +40,26 @@ def location_functions():
         if error is not None:
             flash(error)
         else:
-            #get stuff from database and redirect? or show on this page?
-            return redirect(url_for("location.location_functions"))
+            function_resultproxy = db_session.execute("select list_stopped_vehicles(:datetime, :window)",
+                {"datetime": datetime, "window": timewindow}
+            )
+            vehicles = []
+            db_vehicles = function_resultproxy.fetchall()
+            for row in db_vehicles:
+                d = dict(row.items())
+                rawdata = d["list_stopped_vehicles"].split(",")
+                vehicle = []
+                vehicle.append(rawdata[0][1:])
+                vehicle.append('' if rawdata[1] == "\"\"" else rawdata[1])
+                vehicle.append(rawdata[2][1:19])
+                vehicle.append(round(float(rawdata[3])/60, 1))
+                vehicle.append(rawdata[4])
+                vehicle.append(rawdata[5][:-1])
+                vehicles.append(vehicle)
+            function_resultproxy.close()
+            return render_template("location/index.html", vehicles=vehicles)
 
     return render_template("location/index.html")
-
 
 @bp.route("/time")
 def time():
